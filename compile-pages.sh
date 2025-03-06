52a8aafd0d5815426920df7d2b59c0a2dba458bb
Vahid Ghayoomie <vahidghayoomi@gmail.com>
2025-03-06 22:36:27 +0330

    add pages 11-20

--- compile-pages.sh
+++ compile-pages.sh
@@ -4,13 +4,13 @@
 # for vol in {1..6}; do
 for vol in {1..1}; do
     cd translations/volume-$vol
-    # Build cover
-    pdflatex cover.tex
+    # # Build cover
+    # pdflatex cover.tex
 
-    # Build individual pages
-    for page in pages/*.tex; do
-        pdflatex $page
-    done
+    # # Build individual pages
+    # for page in pages/*.tex; do
+    #     pdflatex $page
+    # done
 
     # Build final volume
     pdflatex EMWv$vol.tex
